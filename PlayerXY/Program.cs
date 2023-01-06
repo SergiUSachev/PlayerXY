caffc8c5a1359c4487c7f980c6d4ad869a9eb89f
Сергей Усачев <usachevplayer@mail.ru>
2023-01-06 02:38:49 +0200

    Добавьте файлы проекта.

---
+++ PlayerXY/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace PlayerXY
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			
+		}
+	}
+	public class Player
+	{
+		private int _x;
+		private int _y;
+
+		public int X { get { return _x; } private set { _x = value; } }
+		public int Y { get { return _y; } private set { _y = value; } }
+
+		public Player(int x, int y) 
+		{
+			x = _x;
+			y = _y;
+
+		}
+
+	}
+
+	public class Renderer
+	{
+
+	}
+}
